6a9aefce82c7bae2f6fab97206e3f9d510fd6fde
Nathan Alderson <nathan.alderson@adtran.com>
2012-09-24 16:57:20 -0500

    adding zshell and oh-my-zshell

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".vimrc .vim/autoload/pathogen.vim"    # list of files/folders to symlink in homedir
+files=".vimrc .vim/autoload/pathogen.vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
