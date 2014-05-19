bd100fa4f29d3528ba66988d39b1d8d06e0b4e8a
Nathan Alderson <nathan.alderson@adtran.com>
2014-05-19 16:17:36 -0500

    Add quotes to echo commands to avoid syntax error on parens

--- setup_new_box.sh
+++ setup_new_box.sh
@@ -1,5 +1,12 @@
 apt-get update
-apt-get -y install vim-gnome git zsh keepass2 xdotool ruby ruby-dev curl openssh-server
+apt-get install vim-gnome
+apt-get install git
+apt-get install zsh
+apt-get install keepass2
+apt-get install xdotool
+apt-get install ruby ruby-dev
+apt-get install curl
+apt-get install openssh-server
 
 curl -L http://install.ohmyz.sh | sh
 
@@ -7,6 +14,6 @@ git clone https://github.com/nathanalderson/dotfiles.git
 dotfiles/setup.sh
 
 echo Other things to do...
-echo  * Download and install dropbox (https://www.dropbox.com/install?os=lnx)
-echo  * Set a global keyboard shortcut for keepass (look under Menu > Preferences > System Settings > Keyboard > ...)
-echo  * Download and install chrome (https://www.google.com/intl/en/chrome/browser/?hl=en&brand=CHFX&utm_campaign=en&utm_source=en-oa-na-us-bk-bng&utm_medium=oa)
+echo  "* Download and install dropbox (https://www.dropbox.com/install?os=lnx)"
+echo  "* Set a global keyboard shortcut for keepass (look under Menu > Preferences > System Settings > Keyboard > ...)"
+echo  "* Download and install chrome (https://www.google.com/intl/en/chrome/browser/?hl=en&brand=CHFX&utm_campaign=en&utm_source=en-oa-na-us-bk-bng&utm_medium=oa)"
