0735c5db92ba4a6d8a760a4f69229a9519f77857
Nathan Alderson <nathan.alderson@gmail.com>
2019-11-15 10:27:01 -0600

    Add script to configure wireguard clients

---
+++ setup-wireguard.sh
@@ -0,0 +1,11 @@
+DEST=${DEST:-/etc/wireguard}
+CONF_FILE=$DEST/wg0.conf
+
+export PRIVATE_KEY=$(wg genkey)
+sudo mkdir -p $DEST
+sudo -E envsubst <wg0.conf.template >$CONF_FILE
+sudo chmod 600 $CONF_FILE
+sudo chown root:root $CONF_FILE
+
+PUBLIC_KEY=$(echo $PRIVATE_KEY | wg pubkey)
+echo Add this public key to the peer: $PUBLIC_KEY
