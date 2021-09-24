ace7f48d6203d7e409cb770b89110fe4d61f0db4
Nathan Alderson <nathan.alderson@gmail.com>
2021-09-24 14:00:38 -0500

    Fix wireguard setup

--- setup-wireguard.sh
+++ setup-wireguard.sh
@@ -6,7 +6,7 @@ sudo mkdir -p $DEST
 sudo touch $CONF_FILE
 sudo chmod 600 $CONF_FILE
 sudo chown root:root $CONF_FILE
-sudo sh -c "sudo -E envsubst <wg0.conf.template >$CONF_FILE"
+sudo -E sh -c "envsubst <wg0.conf.template >$CONF_FILE"
 
 PUBLIC_KEY=$(echo $PRIVATE_KEY | wg pubkey)
 echo Add this public key to the peer: $PUBLIC_KEY
