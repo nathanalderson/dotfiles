0630fe56e8be08d121b57caf1d3e91b38da4c2f4
Nathan Alderson <me@nathanalderson.com>
2022-04-07 20:24:29 -0500

    Update wireguard setup scripts

--- setup-wireguard.sh
+++ setup-wireguard.sh
@@ -1,6 +1,10 @@
 DEST=${DEST:-/etc/wireguard}
 CONF_FILE=$DEST/wg0.conf
 
+echo -n "What is the last octet of the IP address (192.168.100.xxx)? "
+read last_octet
+export IP=192.168.100.$last_octet
+
 export PRIVATE_KEY=$(wg genkey)
 sudo mkdir -p $DEST
 sudo touch $CONF_FILE
