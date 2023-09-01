ebcda25f8319a859b5a2a6eeda38b1fa76035516
Nathan Alderson <me@nathanalderson.com>
2023-09-01 15:11:59 -0500

    Lots of updates/fixes

--- ptt.iex.exs
+++ ptt.iex.exs
@@ -52,6 +52,7 @@ defmodule N do
     nathans_org = UnsecuredOrgs.get_organization_by_name("Nathan's Org")
     SeedHelpers.device_limit(tango_org, 1000)
     SeedHelpers.device_limit(nathans_org, 1000)
+
     SeedHelpers.attempt_insert(
       User.changeset(%User{}, %{
         first_name: "Nathan",
@@ -63,8 +64,10 @@ defmodule N do
         designator: "NAT"
       })
     )
+
     for i <- 1..15 do
       i = i |> Integer.to_string() |> String.pad_leading(2, "0")
+
       SeedHelpers.attempt_insert(
         User.changeset(%User{}, %{
           first_name: "User",
@@ -76,6 +79,7 @@ defmodule N do
         })
       )
     end
+
     # for i <- 1..5 do
     #   i = i |> Integer.to_string() |> String.pad_leading(2, "0")
     #   SeedHelpers.attempt_insert(%Channel{name: "tango channel #{i}", org_id: tango_org.id})
@@ -85,6 +89,7 @@ defmodule N do
     #   SeedHelpers.attempt_insert(%Channel{name: "nathan's channel #{i}", org_id: nathans_org.id})
     # end
     SeedHelpers.set_org_admin(tango_org, N.me())
+
     SeedHelpers.attempt_insert(
       User.changeset(%User{}, %{
         first_name: "Darth",
@@ -96,31 +101,38 @@ defmodule N do
         designator: "DAR"
       })
     )
-    SeedHelpers.attempt_insert(%Device{
+
+    result = SeedHelpers.attempt_insert(%Device{
       device_id: "aae8093a6dd1128e",
       name: "Google Pixel 4a",
       platform: :android,
       platform_version: "13",
       vendor: "Google",
       model: "Pixel 4a",
-      token: "euyxiQavQ9OGqzfZ57HeQ4:APA91bFNfP5lyWtIspT3H8nHKXpFzAK2RGF_l-safrCeoHfn8pvP0im5sTvMNwN0Kitk0hWlnn7Zm9yH6YHU_4isvuFfO0GXmZI-HPLjQZoH5VRm95_KADLE2AxLqFX4fcXn2Q8vvLDf",
-      voip_token: nil,
       app_version: "1.3.7+684",
       release_build: true,
       dnd: true,
       activation_status: :active,
-      user_id: N.me().id,
+      user_id: N.me().id
     })
+    case result do
+      :ok -> nil
+      {:ok, device} ->
+        UnsecuredDevices.update_device_token(%{
+          device_id: device.id,
+          token_value: "d_Dn-zqSQo6RuelR6CDRRh:APA91bERzaOfSKJyC3B-UuVJXDGBPXqkmlRdMj5_FuDs3wB32wFKuKnViVelofnzGrJPzTuqX1Lr8VYZNT3c0rFiS8Ab3s8BUZoLGV7zKWoumFMDUVmRCzLYP72vhe1npFqgKQf4aEEi",
+          token_type: :fcm
+        })
+    end
+
     SeedHelpers.attempt_insert(%Site{
-        name: "Site 01",
-        org_id: tango_org.id,
-        mac_address: "00:00:00:00:00:01",
+      name: "Site 01",
+      org_id: tango_org.id,
+      mac_address: "00:00:00:00:00:01"
     })
   end
 
-  def first_device(user), do:
-    Repo.preload(user, :devices).devices |> Enum.at(0)
-
+  def first_device(user), do: Repo.preload(user, :devices).devices |> Enum.at(0)
 
   def set_log_level(level \\ :info), do: Logger.configure(level: level)
   def dont_truncate(), do: IEx.configure(inspect: [limit: :infinity, printable_limit: :infinity])
@@ -131,6 +143,7 @@ defmodule N do
   def create_sites(count, type \\ :cradlepoint) do
     for i <- 1..count do
       num = String.pad_leading("#{i}", 2, "0")
+
       UnsecuredSites.create_site(%{
         name: "Site #{num}",
         mac_address: "00:00:00:00:00:#{num}",
@@ -144,7 +157,8 @@ defmodule N do
     call_id = call_id || 1
     entities = [{tone.id, :tone}, {tone.channel_id, :channel}, {tone.org_id, :org}]
 
-    1..count |> Enum.flat_map(fn i ->
+    1..count
+    |> Enum.flat_map(fn i ->
       {events, []} = ToneEvents.store_tone_event(entities, :now, tone.id, tone.channel_id, call_id, i)
       events
     end)
@@ -153,37 +167,50 @@ defmodule N do
   def send_messages(count, channel_id, type \\ :standard, text \\ nil, call_id \\ nil, tone_ids \\ []) do
     channel = UnsecuredChannels.get_channel!(channel_id)
     now = Timex.now()
+
     for i <- 1..count do
-      Messaging.send_message(channel, Message.new!(
-        channel_id, Timex.shift(now, seconds: -i), :user, 6890685743094562816, type, text || "Message #{i}", [], %{
-          "callId" => "#{call_id}",
-          "detectedToneIds" => Enum.join(tone_ids, ",")
-        }
-      ))
+      Messaging.send_message(
+        channel,
+        Message.new!(
+          channel_id,
+          Timex.shift(now, seconds: -i),
+          :user,
+          6_890_685_743_094_562_816,
+          type,
+          text || "Message #{i}",
+          [],
+          %{
+            "callId" => "#{call_id}",
+            "detectedToneIds" => Enum.join(tone_ids, ",")
+          }
+        )
+      )
     end
   end
 
   def send_location_update(user, timestamp \\ Timex.now()) do
-      lat = rand_interval(34.769604619877164, 34.66079144385748)
-      long = rand_interval(-86.79176805114027, -86.48162697390367)
-      record = %LatestLocationByOrg.LocationRecord{
-        viewer_org_id: user.org_id,
-        org_id: user.org_id,
-        user_id: user.id,
-        device_id: N.first_device(user).id,
-        coords_latitude: lat,
-        coords_longitude: long,
-        coords_accuracy: 3.0,
-        coords_speed: 0,
-        coords_heading: nil,
-        coords_altitude: nil,
-        battery_level: 1.0,
-        battery_is_charging: false,
-        event: "update",
-        correlation_id: "#{timestamp}",
-        timestamp: timestamp
-      }
-      LatestLocationByOrg.update_location(record)
+    lat = rand_interval(34.769604619877164, 34.66079144385748)
+    long = rand_interval(-86.79176805114027, -86.48162697390367)
+
+    record = %LatestLocationByOrg.LocationRecord{
+      viewer_org_id: user.org_id,
+      org_id: user.org_id,
+      user_id: user.id,
+      device_id: N.first_device(user).id,
+      coords_latitude: lat,
+      coords_longitude: long,
+      coords_accuracy: 3.0,
+      coords_speed: 0,
+      coords_heading: nil,
+      coords_altitude: nil,
+      battery_level: 1.0,
+      battery_is_charging: false,
+      event: "update",
+      correlation_id: "#{timestamp}",
+      timestamp: timestamp
+    }
+
+    LatestLocationByOrg.update_location(record)
   end
 
   def rand_interval(a, b), do: a + (b - a) * :rand.uniform()
