170af87f42280ca499b6987399716691efdb8ba4
Nathan Alderson <me@nathanalderson.com>
2023-12-11 10:59:28 -0600

    Add vscode-shell-integration helper script

---
+++ vscode-shell-integration.zsh
@@ -0,0 +1,29 @@
+# Add paths here to check for vscode shell integration script.
+FILE_PATHS=(
+   "/opt/visual-studio-code/resources/app/out/vs/workbench/contrib/terminal/browser/media/shellIntegration-rc.zsh"
+)
+
+source_if_exists() {
+    local file=$1
+    if [[ -f $file ]]; then
+        . $file
+        return 0
+    fi
+    return 1
+}
+
+for path in "${FILE_PATHS[@]}"; do
+    if source_if_exists $path; then
+        exit 0
+    fi
+done
+
+# If not found, ask vscode where it is (starts nodejs, so a bit slow when starting a terminal)
+FALLBACK_PATH=$(code --locate-shell-integration-path zsh)
+if source_if_exists $FALLBACK_PATH; then
+    exit 0
+else
+    echo "Couldn't find vscode shell integration script"
+    exit 1
+fi
+
