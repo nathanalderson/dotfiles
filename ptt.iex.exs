af36f0aa6390ab7a41c3b3ce2e665dd231a6ff69
Nathan Alderson <me@nathanalderson.com>
2025-12-05 09:18:28 -0600

    Clean up default aliasing; add groups & permissions

--- ptt.iex.exs
+++ ptt.iex.exs
@@ -1,56 +1,46 @@
 alias TangoTango.Core.Messaging
 
-alias TangoTango.Persistence.Repo
-alias TangoTango.Persistence.CassandraRepo
-alias TangoTango.Persistence.Users
-alias TangoTango.Persistence.Users.User
-alias TangoTango.Persistence.Users.Unsecured, as: UnsecuredUsers
-alias TangoTango.Persistence.Users.Devices
-alias TangoTango.Persistence.Users.Devices.Unsecured, as: UnsecuredDevices
-alias TangoTango.Persistence.Users.Device
-alias TangoTango.Persistence.Organizations
-alias TangoTango.Persistence.Organizations.Organization
-alias TangoTango.Persistence.Organizations.OrganizationAdmin
-alias TangoTango.Persistence.Organizations.InteropAgreement
-alias TangoTango.Persistence.Organizations.InteropAgreements
-alias TangoTango.Persistence.RedisUtils
-
-alias TangoTango.Persistence.Organizations.InteropAgreements.Unsecured,
-  as: UnsecuredInteropAgreements
-
 alias TangoTango.Persistence.AuditLog
 alias TangoTango.Persistence.AuditlogManager
 alias TangoTango.Persistence.CallDetailRecords
+alias TangoTango.Persistence.CassandraRepo
 alias TangoTango.Persistence.Channels
 alias TangoTango.Persistence.Channels.Channel
-alias TangoTango.Persistence.Channels.Unsecured, as: UnsecuredChannels
 alias TangoTango.Persistence.ExternalEvents
 alias TangoTango.Persistence.ExternalEvents.ExternalEvent
 alias TangoTango.Persistence.ExternalEvents.ExternalEventConfig
 alias TangoTango.Persistence.ExternalEvents.ExternalEventConfigs
+alias TangoTango.Persistence.Groups
+alias TangoTango.Persistence.Groups.Group
 alias TangoTango.Persistence.LatestLocationByOrg
 alias TangoTango.Persistence.MessageAttachments
 alias TangoTango.Persistence.Messages
 alias TangoTango.Persistence.Messages.Attachment
 alias TangoTango.Persistence.Messages.Message
 alias TangoTango.Persistence.OrganizationFeatures
-alias TangoTango.Persistence.OrganizationFeatures.Unsecured, as: UnsecuredOrgFeatures
-alias TangoTango.Persistence.Organizations.Unsecured, as: UnsecuredOrgs
+alias TangoTango.Persistence.Organizations
+alias TangoTango.Persistence.Organizations.InteropAgreement
+alias TangoTango.Persistence.Organizations.InteropAgreements
+alias TangoTango.Persistence.Organizations.Organization
+alias TangoTango.Persistence.Organizations.OrganizationAdmin
+alias TangoTango.Persistence.Permissions
+alias TangoTango.Persistence.Permissions.Permission
+alias TangoTango.Persistence.RedisUtils
+alias TangoTango.Persistence.Repo
 alias TangoTango.Persistence.SeedHelpers
 alias TangoTango.Persistence.Sites
 alias TangoTango.Persistence.Sites.Site
-alias TangoTango.Persistence.Sites.Unsecured, as: UnsecuredSites
 alias TangoTango.Persistence.ToneEvents
 alias TangoTango.Persistence.Tones
 alias TangoTango.Persistence.Tones.Tone
-alias TangoTango.Persistence.Tones.Unsecured, as: UnsecuredTones
 alias TangoTango.Persistence.Types.Avatar
 alias TangoTango.Persistence.Types.MacAddress
 alias TangoTango.Persistence.Types.PhoneNumber
+alias TangoTango.Persistence.Users
+alias TangoTango.Persistence.Users.Device
+alias TangoTango.Persistence.Users.Devices
 alias TangoTango.Persistence.Users.TokenPermissions
-
-alias TangoTango.Persistence.ExternalEvents.ExternalEventConfigs.Unsecured,
-  as: UnsecuredEventConfigs
+alias TangoTango.Persistence.Users.User
 
 require Ecto.Query
 
